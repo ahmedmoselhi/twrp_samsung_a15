270fae247f32f865fd5c85b12ff639216e0eff92
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2025-10-12 12:59:46 +0300

    major update ** nuke useless manifests & init files ** fix cpu temp and battery paths ** try to fix fastbootd

--- device.mk
+++ device.mk
@@ -2,14 +2,12 @@ LOCAL_PATH := device/samsung/a15
 
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
-# Health HAL
+# Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service \
-    libhealthd.$(PRODUCT_PLATFORM) \
     android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
+    fastbootd \
+	android.hardware.health@2.0-service
+	
 # EROFS Tools
 PRODUCT_HOST_PACKAGES_ENG += \
     liberofs \
